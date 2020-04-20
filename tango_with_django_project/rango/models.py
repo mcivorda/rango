fea26bf3bbc6f64006fc6fa0f16a6fd6559bb3d3
David McIvor <2510167M@student.gla.ac.uk>
2020-04-21 00:59:45 +0100

    Chapter 9: Installed Pillow Page: 154

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     NAME_MAX_LENGTH = 128
@@ -18,6 +20,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Page(models.Model):
     TITLE_MAX_LENGTH = 128
     URL_MAX_LENGTH = 200
@@ -29,3 +32,14 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
